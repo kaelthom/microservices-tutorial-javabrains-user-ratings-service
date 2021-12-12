19cd3bbb43550c8a321cce2283726f50e18db58d
Mickael Thomere <mickael.thomere@gmail.com>
2021-12-12 19:05:03 +0100

    Microservices Tutorial : add application name + explicit declaration of the application as Eureka client

--- src/main/java/com/microservicestutorial/movieratingservice/MovieRatingServiceApplication.java
+++ src/main/java/com/microservicestutorial/movieratingservice/MovieRatingServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservicestutorial.movieratingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // not mandatory
 public class MovieRatingServiceApplication {
 
     public static void main(String[] args) {
